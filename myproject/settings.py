0c9ede11390fd041a823c0b9a14877223daaeb73
Kim Junghun <jeonghun9326@naver.com>
2025-05-16 09:48:09 +0900

    직급 직책 직위 등록오류수정

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u6sb$(w-7kmcrgf4!ht07w(!nv^is&_(g(sq!!^au=cr-d@92l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.152.162.98', 'localhost']
 
 
 # Application definition
